1791cef4995c6effd3d350829f31f45533477958
Leonard907 <ou_litu@163.com>
2022-06-14 22:48:34 +0800

    add mock prepare file

---
+++ evaluator/prepare.sh
@@ -0,0 +1,9 @@
+python prepare_submission.py \
+--gov_report_file ~/scrolls_ilcc/baselines/experiments/mock_gov_report.json \
+--summ_screen_file /home/s1970716/scrolls_ilcc/baselines/outputs/facebook-bart-base_256_32_0.0001_4096_scrolls_qmsum_director-habit-5_scrolls_summ_screen_fd_bathroom-candidate-19/generated_predictions.json \
+--qmsum_file ~/scrolls_ilcc/baselines/outputs/facebook-bart-base_256_32_0.0001_4096_scrolls_qmsum_director-habit-5_scrolls_qmsum_car-join-6/generated_predictions.json \
+--narrative_qa_file ~/scrolls_ilcc/baselines/experiments/mock_nqa.json \
+--qasper_file /home/s1970716/scrolls_ilcc/baselines/outputs/facebook-bart-base_256_32_0.0001_4096_scrolls_qmsum_director-habit-5_scrolls_qasper_light-traffic-23/generated_predictions.json \
+--quality_file /home/s1970716/scrolls_ilcc/baselines/outputs/facebook-bart-base_256_32_0.0001_4096_scrolls_qmsum_director-habit-5_scrolls_quality_player-degree-24/generated_predictions.json \
+--contract_nli_file /home/s1970716/scrolls_ilcc/baselines/outputs/facebook-bart-base_256_32_0.0001_4096_scrolls_qmsum_director-habit-5_scrolls_contract_nli_age-tough-25/generated_predictions.json \
+--output_dir .
