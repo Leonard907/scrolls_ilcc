f00ce035bbc19eedd51d95ff1f02a154a70b005c
Litu Ou <s1970716@arnold.inf.ed.ac.uk>
2022-06-15 16:16:56 +0100

    add full sh script

--- baselines/experiments/qmsum_256-bart_test.sh
+++ baselines/experiments/qmsum_256-bart_test.sh
@@ -9,18 +9,19 @@
 #SBATCH --time=48:00:00
 #SBATCH --array=0
 
-output_path=experiments/output/qmsum_led-1024
+model=qmsum_led-4096
+output_path=experiments/output/${model}
 
-export XDG_CACHE_HOME=experiments/data/qmsum_led-1024
+export XDG_CACHE_HOME=experiments/data/${model}
 
-python scripts/execute.py scripts/commands/finetune.py qmsum_led-1024_data
+python scripts/execute.py scripts/commands/finetune.py ${model}_data
 
-python scripts/execute.py scripts/commands/finetune.py qmsum_led-1024 \
+python scripts/execute.py scripts/commands/finetune.py ${model} \
   --output_dir=${output_path}
 
-python scripts/execute.py scripts/commands/generate.py qmsum_5led-1024_test --checkpoint_path ${output_path} --output_dir ${output_path}
+python scripts/execute.py scripts/commands/generate.py ${model}_test --checkpoint_path ${output_path} --output_dir ${output_path}
 
-python prepare_submission.py \
+python ~/scrolls_ilcc/evaluator/prepare_submission.py \
 --gov_report_file ~/scrolls_ilcc/baselines/experiments/mock_gov_report.json \
 --summ_screen_file /home/s1970716/scrolls_ilcc/baselines/outputs/facebook-bart-base_256_32_0.0001_4096_scrolls_qmsum_director-habit-5_scrolls_summ_screen_fd_bathroom-candidate-19/generated_predictions.json \
 --qmsum_file ${output_path}/generated_predictions.json \
@@ -28,4 +29,4 @@ python prepare_submission.py \
 --qasper_file /home/s1970716/scrolls_ilcc/baselines/outputs/facebook-bart-base_256_32_0.0001_4096_scrolls_qmsum_director-habit-5_scrolls_qasper_light-traffic-23/generated_predictions.json \
 --quality_file /home/s1970716/scrolls_ilcc/baselines/outputs/facebook-bart-base_256_32_0.0001_4096_scrolls_qmsum_director-habit-5_scrolls_quality_player-degree-24/generated_predictions.json \
 --contract_nli_file /home/s1970716/scrolls_ilcc/baselines/outputs/facebook-bart-base_256_32_0.0001_4096_scrolls_qmsum_director-habit-5_scrolls_contract_nli_age-tough-25/generated_predictions.json \
---output_dir .
+--output_dir ${output_path}
