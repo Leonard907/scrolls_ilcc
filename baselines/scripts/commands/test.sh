b4332c0e30c64490d63f645256ac1b907cac490e
oulitu <oulitu@bytedance.com>
2022-06-21 23:45:41 +0800

    add sh for sbatch

---
+++ baselines/scripts/commands/test.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH --output=/home/%u/slogs/sl-%x-%A-%a.out
+#SBATCH --error=/home/%u/slogs/sl-%x-%A-%a.out
+#SBATCH --job-name=qmsum_256-bart
+#SBATCH --nodes=1
+#SBATCH --gres=gpu:2
+#SBATCH --mem=16g
+#SBATCH --cpus-per-task=4
+#SBATCH --time=48:00:00
+#SBATCH --array=0
+
+python scripts/execute.py scripts/commands/finetune.py qmsum_longt5-local --source_prefix "summarize: "
