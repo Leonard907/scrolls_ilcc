81bab660c7182f4942a5dbd28c7fee42fa6ed357
oulitu <oulitu@bytedance.com>
2022-06-23 18:09:41 +0800

    update

---
+++ evaluator/prepare_mock.sh
@@ -0,0 +1,9 @@
+python prepare_submission.py \
+--gov_report_file ~/scrolls_ilcc/baselines/experiments/mock_gov_report.json \
+--summ_screen_file ~/scrolls_ilcc/baselines/experiments/mock_sfd.json \
+--qmsum_file ~/temp/generated_predictions.json \
+--narrative_qa_file ~/scrolls_ilcc/baselines/experiments/mock_nqa.json \
+--qasper_file ~/scrolls_ilcc/baselines/experiments/mock_qasper.json \
+--quality_file ~/scrolls_ilcc/baselines/experiments/mock_qual.json \
+--contract_nli_file ~/scrolls_ilcc/baselines/experiments/mock_cnli.json \
+--output_dir .
