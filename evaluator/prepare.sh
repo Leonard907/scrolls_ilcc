bf6191fcb40cbe8ce271a7b6c606778443a25f73
oulitu <oulitu@bytedance.com>
2022-06-25 13:37:13 +0800

    update prepare

--- evaluator/prepare.sh
+++ evaluator/prepare.sh
@@ -1,9 +1,9 @@
 python prepare_submission.py \
 --gov_report_file ~/scrolls_ilcc/baselines/experiments/mock_gov_report.json \
---summ_screen_file /home/s1970716/scrolls_ilcc/baselines/outputs/facebook-bart-base_256_32_0.0001_4096_scrolls_qmsum_director-habit-5_scrolls_summ_screen_fd_bathroom-candidate-19/generated_predictions.json \
---qmsum_file ~/scrolls_ilcc/baselines/outputs/_scrolls_qmsum_product-drunk-29/generated_predictions.json \
+--summ_screen_file ~/scrolls_ilcc/baselines/experiments/mock_sfd.json \
+--qmsum_file ~/temp/generated_predictions.json \
 --narrative_qa_file ~/scrolls_ilcc/baselines/experiments/mock_nqa.json \
---qasper_file /home/s1970716/scrolls_ilcc/baselines/outputs/facebook-bart-base_256_32_0.0001_4096_scrolls_qmsum_director-habit-5_scrolls_qasper_light-traffic-23/generated_predictions.json \
---quality_file /home/s1970716/scrolls_ilcc/baselines/outputs/facebook-bart-base_256_32_0.0001_4096_scrolls_qmsum_director-habit-5_scrolls_quality_player-degree-24/generated_predictions.json \
---contract_nli_file /home/s1970716/scrolls_ilcc/baselines/outputs/facebook-bart-base_256_32_0.0001_4096_scrolls_qmsum_director-habit-5_scrolls_contract_nli_age-tough-25/generated_predictions.json \
+--qasper_file ~/scrolls_ilcc/baselines/experiments/mock_qasper.json \
+--quality_file ~/scrolls_ilcc/baselines/experiments/mock_qual.json \
+--contract_nli_file ~/scrolls_ilcc/baselines/experiments/mock_cnli.json \
 --output_dir .
