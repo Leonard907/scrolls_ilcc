9d786153462404f620ae45d5f18aada693030353
Yao Fu <hpcfu1@login-e-11.data.cluster>
2022-10-19 10:18:37 +0100

    modify test files

--- baselines/test_gen.sh
+++ baselines/test_gen.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-#SBATCH --output=/home/%u/longt5_gov_gen_log/knn_v1.3_8192_epoch_10.out
-#SBATCH --error=/home/%u/longt5_gov_gen_log/knn_v1.3_8192_epoch_110.out
+#SBATCH --output=/home/%u/longt5_gov_gen_log/knn_v1.3_8192_epoch_10_test.out
+#SBATCH --error=/home/%u/longt5_gov_gen_log/knn_v1.3_8192_epoch_10_test.out
 #SBATCH --job-name=gov_longt5-local
 #SBATCH --nodes=1
 #SBATCH --gres=gpu:1
@@ -10,4 +10,4 @@
 #SBATCH --array=0
 
 export XDG_CACHE_HOME=/home/$USER/rds/hpc-work/scrolls/scrolls_ilcc/scrolls_data/gov_report
-python scripts/execute.py scripts/commands/generate.py gov_report_longt5-local_validation --checkpoint_path outputs/google-long-t5-tglobal-base_8192_32_0.001_8192_scrolls_gov_report_efficiency-stay-285
+python scripts/execute.py scripts/commands/generate.py gov_report_longt5-local_test --checkpoint_path outputs/google-long-t5-tglobal-base_8192_32_0.001_8192_scrolls_gov_report_efficiency-stay-285
--- evaluator/prepare_mock.sh
+++ evaluator/prepare_mock.sh
@@ -1,9 +1,9 @@
 python prepare_submission.py \
---gov_report_file ~/scrolls_ilcc/baselines/experiments/mock_gov_report.json \
---summ_screen_file ~/scrolls_ilcc/baselines/experiments/mock_sfd.json \
---qmsum_file ~/temp/generated_predictions.json \
---narrative_qa_file ~/scrolls_ilcc/baselines/experiments/mock_nqa.json \
---qasper_file ~/scrolls_ilcc/baselines/experiments/mock_qasper.json \
---quality_file ~/scrolls_ilcc/baselines/experiments/mock_qual.json \
---contract_nli_file ~/scrolls_ilcc/baselines/experiments/mock_cnli.json \
+--gov_report_file ../baselines/outputs/google-long-t5-tglobal-base_8192_32_0.001_8192_scrolls_gov_report_efficiency-stay-285_scrolls_gov_report_brush-forever-287/generated_predictions_val.json \
+--summ_screen_file ../baselines/experiments/mock_sfd.json \
+--qmsum_file ../../temp/generated_predictions.json \
+--narrative_qa_file ../baselines/experiments/mock_nqa.json \
+--qasper_file ../baselines/experiments/mock_qasper.json \
+--quality_file ../baselines/experiments/mock_qual.json \
+--contract_nli_file ../baselines/experiments/mock_cnli.json \
 --output_dir .
